48a588bfd7a72c5e17229a36fb1a170d16a09f79
shuyang-he <she26@wustl.edu>
2020-02-03 02:06:36 -0800

    update

--- app.js
+++ app.js
@@ -3,21 +3,33 @@ const app = express();
 const port = 8080;
 const indexRouter = require('./routes/index');
 const WebSocket = require('ws');
+const fs = require('fs');
+const path = require('path');
 
 const wss = new WebSocket.Server({ port: 8080 });
 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    console.log('server received: %s', message);
-    if (message > 9) {
-      ws.send('Server: This number has two digits.');
-    } else {
-      ws.send('Server: This number has one digit.');
+    const data = JSON.parse(message.toString());
+    console.log(data);
+    const {command, path} = data;
+    
+    if (command === 'load') {
+      fs.readFile(path, (err, res) => {
+        ws.send(res);
+      });
     }
+    // if (command === 'subtract') {
+    //   const res = x - y;
+    //   ws.send(res);
+    // }
   });
 });
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}!`);
